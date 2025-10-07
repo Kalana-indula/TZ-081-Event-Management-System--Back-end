0f72d078819aa7c599d437ef300f3537e4605b10
Kalana-indula <kalanasmedia@outlook.com>
2025-10-07 16:50:43 +0530

    security configurations added

--- src/main/java/com/eventwisp/app/repository/AdminRepository.java
+++ src/main/java/com/eventwisp/app/repository/AdminRepository.java
@@ -4,6 +4,13 @@ import com.eventwisp.app.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin,Long> {
+
+    //find admin by email address
+    Optional<Admin> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
